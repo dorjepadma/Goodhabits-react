d8aced71070ab9506bf30f6a5aee059dfa134d38
dorjepadma <dorjepadma@gmail.com>
2020-05-14 08:30:53 -0700

    API link created

--- src/services/GoodHabitAPI.js
+++ src/services/GoodHabitAPI.js
@@ -1,4 +1,11 @@
 export const postHabit = habit => {
-  return fetch('http://localhost7890/api');
+  return fetch('http://localhost7890/api/v1,goodhabits', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(habit)
+  })
+    .then(res => res.json());
 };
 
